56306540c4e0c2fc0a8066e17016384b5f19560b
Gabriel Plinio <gabriel.machado.tblh@gmail.com>
2025-08-07 20:54:35 -0300

    Track dbConnect.js again (using env variables)

---
+++ src/config/dbConnect.js
@@ -0,0 +1,10 @@
+import "dotenv/config"
+import mongoose, {mongo} from "mongoose";
+
+async function conectaNaDabase() {
+	mongoose.connect(process.env.DB_CONNECTION_STRING);
+
+	return mongoose.connection;
+}
+
+export default conectaNaDabase;
