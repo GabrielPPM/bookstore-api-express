7ebde013204c9462895daefe70391ff7f3387e4b
Machado Gabriel (CaP/ETS2) <mga6ca@bosch.com>
2025-08-12 08:09:27 -0300

    fix.: removed () from ErroValidacao extends ErroBase()

--- src/erros/erroDeValidacao.js
+++ src/erros/erroDeValidacao.js
@@ -1,6 +1,6 @@
 import ErroBase from "./erroBase.js";
 
-class ErroDeValidacao extends ErroBase(){
+class ErroDeValidacao extends ErroBase{
     constructor(){
         super("Um ou mais campos não cumprem os requisitos de validação", 400);
     }
